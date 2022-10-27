5ca2be4620f5fce4f0560f549fcca7a4fc848a89
Fernando Aguilar <sparkmanperspective@gmail.com>
2022-10-26 20:27:36 -0400

    Included StreamWriter functionality

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 
 namespace NumberTracker
 {
@@ -44,6 +45,11 @@ namespace NumberTracker
                     numbers.Add(number);
                 }
             }
+
+            var fileWriter = new StreamWriter("numbers.csv");
+
+
+
         }
     }
 }
