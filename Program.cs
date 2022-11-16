db379188f956eb0f06bc485c96582fdb3f658cec
Fernando Aguilar <sparkmanperspective@gmail.com>
2022-11-16 15:28:58 -0500

    Wrapped the TextReader parts in one If statement

--- Program.cs
+++ Program.cs
@@ -23,36 +23,45 @@ namespace NumberTracker
         {
             Console.WriteLine("Welcome to Number Tracker");
 
-            //Part 2: After we put the Write together, we then add a READ stream.
-            //so we create a file reader to read from numbers.csv.
-            //var fileReader = new StreamReader("numbers.csv");
+            //Part 4:
+            var numbers = new List<int>();
 
-            //Part 3: use TextReader instead of a fileReader. This also solves cases where there is no file.csv found.
-            // Algorithm: Detect if there is no file.
-            // • If there is a file, use a StreamReader pointing to the file
-            // • If there isn't a file, use a StringReader with an empty string
-            TextReader reader;
+            //We wrapped all of the code below to improve
             if (File.Exists("numbers.csv"))
             {
-                //Assign StreamReader to read from the file
-                reader = new StreamReader("numbers.csv");
+
+                //Part 2: After we put the Write together, we then add a READ stream.
+                //so we create a file reader to read from numbers.csv.
+                //var fileReader = new StreamReader("numbers.csv");
+
+                //Part 3: use TextReader instead of a fileReader. This also solves cases where there is no file.csv found.
+                // Algorithm: Detect if there is no file.
+                // • If there is a file, use a StreamReader pointing to the file
+                // • If there isn't a file, use a StringReader with an empty string
+                TextReader reader;
+                if (File.Exists("numbers.csv"))
+                {
+                    //Assign StreamReader to read from the file
+                    reader = new StreamReader("numbers.csv");
+                }
+                else
+                {
+                    //Assign StringReader to read from an empty string. And parse the csv from this empty string.
+                    reader = new StringReader("");
+                }
+
+                //Then we follow the reader by creating a configuration that indicates this CSV file has no header
+                var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+                {
+                    //Tell the reader not to interpret the first row as a "header" since it is just the first number.
+                    HasHeaderRecord = false,//???????
+                };
+                var csvReader = new CsvReader(reader, config);
+
+                //Create our List<int> but from *READING* the data from the file!!
+                //replace our empty list of numbers with the populated version!
+                numbers = csvReader.GetRecords<int>().ToList();
             }
-            else
-            {
-                //Assign StringReader to read from an empty string. And parse the csv from this empty string.
-                reader = new StringReader("");
-            }
-
-            //Then we follow the reader by creating a configuration that indicates this CSV file has no header
-            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
-            {
-                //Tell the reader not to interpret the first row as a "header" since it is just the first number.
-                HasHeaderRecord = false,
-            };
-            var csvReader = new CsvReader(reader, config);
-
-            var numbers = csvReader.GetRecords<int>().ToList();
-
             ////////////////////////////////////////////Part 1 below:
             // Creates a list of numbers we will be tracking
             //var numbers = new List<int>();
